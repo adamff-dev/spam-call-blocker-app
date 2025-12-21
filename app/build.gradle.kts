cf52c9352f231115dd948947c7025bb8981ecab4
Adam <adamff.me@gmail.com>
2025-12-21 20:35:34 +0100

    build.gradle.kts: Bump version to fix versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 25
-        versionName = "2.5.3"
+        versionCode = 26
+        versionName = "2.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
